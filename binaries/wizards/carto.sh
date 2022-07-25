fbf3c142b7ccc969b8f9acc1fe556edf7a97bba3
Ali Nahid <alinahid@gmail.com>
2022-07-25 10:58:57 +1000

    carto wip

--- binaries/wizards/carto.sh
+++ binaries/wizards/carto.sh
@@ -5,9 +5,168 @@ source $HOME/binaries/scripts/returnOrexit.sh
 source $HOME/binaries/scripts/color-file.sh
 
 source $HOME/binaries/tapscripts/build-carto-values-file.sh
+source $HOME/binaries/scripts/extract-and-take-input.sh
 
+function resolveServiceAccountClusterRolesAndBindings () {
+    local cartoValuesFile=$1 # REQUIRED
+    local cartoRBAC=$HOME/binaries/templates/carto-rbac.yaml
+
+    printf "\nStarting ClusterRole and ClusterBinding configuring...\n"
+
+    if [[ ! -f $cartoValuesFile ]]
+    then
+        printf "\n${redcolor}Error: CartoValuesFile is needed for fulfilling pre-requisite of clusterrole and clusterrolebinding.${normalcolor}\n"
+    fi
+    local serviceAccountName=$(cat $cartoValuesFile | yq -e '.service_account.name' --no-colors)
+    local serviceAccountNamespace=$(cat $cartoValuesFile | yq -e '.service_account.namespace' --no-colors)
+    if [[ -z $serviceAccountName || -z $serviceAccountNamespace ]]
+    then
+        printf "\n${redcolor}Error: ValuesFile does not container service account name or its namespace.${normalcolor}\n"
+    fi
+
+    printf "Checking SA: $serviceAccountName..."
+    local isexist=$(kubectl describe sa $serviceAccountName -n $serviceAccountNamespace)
+    if [[ -z $isexist ]]
+    then
+        printf "NOT FOUND. Creating new...\n"
+        kubectl create sa $serviceAccountName -n $serviceAccountNamespace || returnOrexit || return 1
+        printf "CREATED\n"
+    else
+        printf "FOUND. Continuing...\n"
+    fi
+
+    printf "Checking ClusterRole: carto-clusterrole..."
+    isexist=$(kubectl describe clusterrole carto-clusterrole)
+    if [[ -z $isexist ]]
+    then
+        printf "NOT FOUND. Creating NEW and binding with sa $serviceAccountName in ns: $serviceAccountNamespace...\n"
+        kubectl apply -f <(ytt --ignore-unknown-comments -f $cartoValuesFile -f $cartoRBAC) || returnOrexit || return 1
+        printf "CREATED and BOUNDED with sa $serviceAccountName in ns: $serviceAccountNamespace\n"
+    else
+        printf "FOUND. binding with sa $serviceAccountName in ns: $serviceAccountNamespace...\n"
+        kubectl apply -f <(ytt --ignore-unknown-comments -f $cartoValuesFile -f $cartoRBAC) || returnOrexit || return 1
+        printf "BOUNDED with sa $serviceAccountName in ns: $serviceAccountNamespace\n"
+    fi
+}
+
+
+function resolveTekton () {
+    printf "\nStarting Tekton configuration...\n"
+    sleep 2
+    printf "Looking for ns: tekton-pipelines..."
+    sleep 1
+    local isexist=$(kubectl get ns | grep -i tekton-pipelines)
+    if [[ -z $isexist ]]
+    then
+        printf "NOT FOUND.\n"
+    else
+        printf "FOUND.\n"
+    fi
+    local confirmed=''
+    while true; do
+        read -p "Would you like to install tekton (needed for test, git-ops operations)? [y/n] " yn
+        case $yn in
+            [Yy]* ) printf "you confirmed yes\n"; confirmed='y'; break;;
+            [Nn]* ) printf "You confirmed no.\n"; confirmed='n'; break;;
+            * ) echo "Please answer y or n.";
+        esac
+    done    
+    if [[ $confirmed == 'y' ]]
+    then
+        printf "Installing Tekton....\n"
+        local TEKTON_VERSION=0.30.0 
+        kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/previous/v$TEKTON_VERSION/release.yaml
+        printf "\nTekton installation...COMPLETE\n"
+        sleep 2
+    fi
+}
+
+function resolveTektonTaskForTest () {
+    local confirmed=''
+    while true; do
+        read -p "Would you like create tekton-task for maven test (needed for supply-chain with test)? [y/n] " yn
+        case $yn in
+            [Yy]* ) printf "you confirmed yes\n"; confirmed='y'; break;;
+            [Nn]* ) printf "You confirmed no.\n"; confirmed='n'; break;;
+            * ) echo "Please answer y or n.";
+        esac
+    done    
+    if [[ $confirmed == 'y' ]]
+    then
+        printf "Creating TektonTask: for maven-test for test...\n"
+        local templateFile=$HOME/binaries/templates/carto-test.tekton-task.maven.template
+        local baseFile=$HOME/configs/carto/carto-test.tekton-task.maven.yaml
+        cp $templateFile $baseFile
+        extractVariableAndTakeInputUsingCustomPromptsFile "prompts-for-variables.carto.json" $baseFile
+        kubectl create -f $baseFile
+        printf "\nTektonTask create...COMPLETE\n"
+        sleep 2
+    fi
+}
+
+function resolveTektonTaskForGitWriter () {
+    local confirmed=''
+    while true; do
+        read -p "Would you like to install tekton git-cli task from tekton catalog (needed for git-ops)? [y/n] " yn
+        case $yn in
+            [Yy]* ) printf "you confirmed yes\n"; confirmed='y'; break;;
+            [Nn]* ) printf "You confirmed no.\n"; confirmed='n'; break;;
+            * ) echo "Please answer y or n.";
+        esac
+    done    
+    if [[ $confirmed == 'y' ]]
+    then
+        printf "Installing Tekton git cli....\n"
+        kapp deploy --yes -a tekton-git-cli -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-cli/0.2/git-cli.yaml
+        printf "\nTekton git cli...COMPLETE\n"
+        sleep 2       
+    fi
+    confirmed=''
+    while true; do
+        read -p "Would you like create tekton-task for git-writer (needed for git-ops)? [y/n] " yn
+        case $yn in
+            [Yy]* ) printf "you confirmed yes\n"; confirmed='y'; break;;
+            [Nn]* ) printf "You confirmed no.\n"; confirmed='n'; break;;
+            * ) echo "Please answer y or n.";
+        esac
+    done    
+    if [[ $confirmed == 'y' ]]
+    then
+        printf "Creating TektonTask: for git-writer for git-ops...\n"
+        local templateFile=$HOME/binaries/templates/carto-gitwriter.tekton-task.configwriter.template
+        local baseFile=$HOME/configs/carto/carto-gitwriter.tekton-task.configwriter.yaml
+        cp $templateFile $baseFile
+        extractVariableAndTakeInputUsingCustomPromptsFile "prompts-for-variables.carto.json" $baseFile
+        kubectl create -f $baseFile
+        printf "\nTektonTask create...COMPLETE\n"
+        sleep 2
+    fi
+}
+
+
+
+function cartoTemplateWizardPrompts () {
+
+    printf "\n\n${yellowcolor}Cartographer Supply Chain templating wizard. It will perform the followings..."
+    printf "\n\t 1. Create/Use Service Account, ClusterRole and ClusterRoleBinding (carto supply chain needs appropriate permissions to run)"
+    printf "\n\t 2. Create/Use Various cartographer templates. This wizard will walk you through."
+    printf "\n\t 3. Install Tekton.dev if needed (This wizard uses tekton task to for creating test, git-ops operation if your supply chain contains test and/or git-ops)"
+    printf "\n\t 4. Create tekton task for test if needed (This wizard uses tekton task to for creating test. eg: maven test)"
+    printf "\n\t 5. Create tekton task for Git Writer if needed (This wizard uses tekton task for writing configmap to git for git-ops)"
+    
+    printf "\n${normalcolor}\n\n"
+
+    sleep 2
+}
 
 function createCartoTemplates () {
+    cartoTemplateWizardPrompts
+
+
+
+    local isTektonRequired='n'
+    local isTektonTestRequired='n'
+    local isTektonGitWriterRequired='n'
 
     local cartoDir=$HOME/configs/carto
     buildCartoValuesFile "templates" $cartoDir "/tmp/cartoValuesFilePath"
@@ -23,31 +182,40 @@ function createCartoTemplates () {
         printf "\n${redcolor}Error: carto values file not found. ${normalcolor}\n"
         returnOrexit || return 1
     fi
+
+    resolveServiceAccountClusterRolesAndBindings
+
     mkdir /tmp/carto
-    local isexist=$(cat $cartoValuesFile | yq -e '.src' --no-colors)
+    local isexist=''
+
+    isexist=$(cat $cartoValuesFile | yq -e '.src' --no-colors)
     if [[ -n $isexist && $isexist != null ]]
     then
-        cp $HOME/binaries/templates/carto-clustersource.template /tmp/carto/carto-clustersource.yaml && ytt -f $cartoValuesFile -f /tmp/carto/carto-clustersource.yaml > $cartoDir/carto-clustersource.yaml
+        cp $HOME/binaries/templates/carto-clustersource.template /tmp/carto/carto-clustersource.yaml && ytt --ignore-unknown-comments -f $cartoValuesFile -f /tmp/carto/carto-clustersource.yaml > $cartoDir/carto-clustersource.yaml
     fi
-    local isexist=$(cat $cartoValuesFile | yq -e '.test' --no-colors)
+    isexist=$(cat $cartoValuesFile | yq -e '.test' --no-colors)
     if [[ -n $isexist && $isexist != null ]]
     then
-        cp $HOME/binaries/templates/carto-test.template /tmp/carto/carto-test.yaml && ytt -f $cartoValuesFile -f /tmp/carto/carto-test.yaml > $cartoDir/carto-test.yaml
+        isTektonRequired='y'
+        isTektonTestRequired='y'
+        cp $HOME/binaries/templates/carto-test.template /tmp/carto/carto-test.yaml && ytt --ignore-unknown-comments -f $cartoValuesFile -f /tmp/carto/carto-test.yaml > $cartoDir/carto-test.yaml
     fi
-    local isexist=$(cat $cartoValuesFile | yq -e '.kpack' --no-colors)
+    isexist=$(cat $cartoValuesFile | yq -e '.kpack' --no-colors)
     if [[ -n $isexist && $isexist != null ]]
     then
-        cp $HOME/binaries/templates/carto-clusterimage-kpack.template /tmp/carto/carto-clusterimage-kpack.yaml && ytt -f $cartoValuesFile -f /tmp/carto/carto-clusterimage-kpack.yaml > $cartoDir/carto-clusterimage-kpack.yaml
+        cp $HOME/binaries/templates/carto-clusterimage-kpack.template /tmp/carto/carto-clusterimage-kpack.yaml && ytt --ignore-unknown-comments -f $cartoValuesFile -f /tmp/carto/carto-clusterimage-kpack.yaml > $cartoDir/carto-clusterimage-kpack.yaml
     fi
-    local isexist=$(cat $cartoValuesFile | yq -e '.knative' --no-colors)
+    isexist=$(cat $cartoValuesFile | yq -e '.knative' --no-colors)
     if [[ -n $isexist && $isexist != null ]]
     then
-        cp $HOME/binaries/templates/carto-knative-configmap.template /tmp/carto/carto-knative-configmap.yaml && ytt -f $cartoValuesFile -f /tmp/carto/carto-knative-configmap.yaml > $cartoDir/carto-knative-configmap.yaml
+        cp $HOME/binaries/templates/carto-knative-configmap.template /tmp/carto/carto-knative-configmap.yaml && ytt --ignore-unknown-comments -f $cartoValuesFile -f /tmp/carto/carto-knative-configmap.yaml > $cartoDir/carto-knative-configmap.yaml
     fi
-    local isexist=$(cat $cartoValuesFile | yq -e '.gitwriter' --no-colors)
+    isexist=$(cat $cartoValuesFile | yq -e '.gitwriter' --no-colors)
     if [[ -n $isexist && $isexist != null ]]
     then
-        cp $HOME/binaries/templates/carto-gitwriter.template /tmp/carto/carto-gitwriter.yaml && ytt -f $cartoValuesFile -f /tmp/carto/carto-gitwriter.template > $cartoDir/carto-gitwriter.yaml
+        isTektonRequired='y'
+        isTektonGitWriterRequired='y'
+        cp $HOME/binaries/templates/carto-gitwriter.template /tmp/carto/carto-gitwriter.yaml && ytt --ignore-unknown-comments -f $cartoValuesFile -f /tmp/carto/carto-gitwriter.template > $cartoDir/carto-gitwriter.yaml
     fi
     printf "\nfile generation complete...\n"
     local confirmed=''
@@ -61,34 +229,50 @@ function createCartoTemplates () {
     done    
     if [[ $confirmed == 'y' ]]
     then
-        local isexist=$(cat $cartoValuesFile | yq -e '.src' --no-colors)
+        isexist=$(cat $cartoValuesFile | yq -e '.src' --no-colors)
         if [[ -n $isexist && $isexist != null ]]
         then
             kubectl create -f $cartoDir/carto-clustersource.yaml
         fi
-        local isexist=$(cat $cartoValuesFile | yq -e '.test' --no-colors)
+        isexist=$(cat $cartoValuesFile | yq -e '.test' --no-colors)
         if [[ -n $isexist && $isexist != null ]]
         then
             kubectl create -f $cartoDir/carto-test.yaml
         fi
-        local isexist=$(cat $cartoValuesFile | yq -e '.kpack' --no-colors)
+        isexist=$(cat $cartoValuesFile | yq -e '.kpack' --no-colors)
         if [[ -n $isexist && $isexist != null ]]
         then
             kubectl create -f $cartoDir/carto-clusterimage-kpack.yaml
         fi
-        local isexist=$(cat $cartoValuesFile | yq -e '.knative' --no-colors)
+        isexist=$(cat $cartoValuesFile | yq -e '.knative' --no-colors)
         if [[ -n $isexist && $isexist != null ]]
         then
             kubectl create -f $cartoDir/carto-knative-configmap.yaml
         fi
-        local isexist=$(cat $cartoValuesFile | yq -e '.gitwriter' --no-colors)
+        isexist=$(cat $cartoValuesFile | yq -e '.gitwriter' --no-colors)
         if [[ -n $isexist && $isexist != null ]]
         then
             kubectl create -f $cartoDir/carto-gitwriter.yaml
         fi
         printf "\n${greencolor}Carto CRDs created...COMPLETED${normalcolor}\n"
     fi
+
     printf "Cleaing..."
     rm -r /tmp/carto/ && printf "DONE" || printf "FAILED"
     printf "\n"
+
+    if [[ $isTektonRequired == 'y' ]]
+    then
+        resolveTekton
+    fi
+    if [[ $isTektonTestRequired == 'y' ]]
+    then
+        resolveTektonTaskForTest
+    fi
+    if [[ $isTektonGitWriterRequired == 'y' ]]
+    then
+        resolveTektonTaskForGitWriter
+    fi
+
+    printf "\nCarto Template Wizard...COMPLETE\n"
 }
